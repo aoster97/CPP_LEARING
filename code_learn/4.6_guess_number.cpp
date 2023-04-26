2b8acb61b6548c558e78e9e0ea393870d2351db4
aoeroz <1249601502@qq.com>
2023-04-27 07:29:42 +0800

    20230427

--- code_learn/4.6_guess_number.cpp
+++ code_learn/4.6_guess_number.cpp
@@ -6,22 +6,21 @@ int main()
     int a;
     int num = rand() % 100 + 1;
     // cout << num << endl;
-    cout << "请输入您猜的数字" << endl;
-    cin >> a;
     while (a != 9999)
     {
+        cout << "请输入您猜的数字(输入999结束):" << endl;
+        cin >> a;
         if (a > num)
         {
-            cout << "猜测过大" << endl;
+            cout << "猜测过大." << endl;
         }
         else if (a = num)
         {
-            cout << "数字相等" << endl;
-            break;
+            cout << "数字相等,恭喜猜对了!!!" << endl;
         }
         else if (a < num)
         {
-            cout << "猜测过小" << endl;
+            cout << "猜测过小." << endl;
         }
     }
 }
