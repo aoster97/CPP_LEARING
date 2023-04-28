de85f166961d1dd5fc508af0a235c37aef74c93d
aoeroz <1249601502@qq.com>
2023-04-29 00:39:43 +0800

    20230429

--- code_learn/4.10_knock_desk.cpp
+++ code_learn/4.10_knock_desk.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main()
 {
-    
+    for (int a = 1; a <= 100; a++)
+    {
+        if (a % 10 == 7 || a / 10 == 7 || a % 7 == 0)
+        {
+            cout << "敲桌子" << endl;
+        }
+        cout << a << endl;
+    }
 }
