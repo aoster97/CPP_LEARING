cd694d04ff41982a5514378b38bb0d7d7b7a98f7
aoeroz <1249601502@qq.com>
2023-05-29 09:30:27 +0800

    20230529

---
+++ code_learn/9.0_Global_area.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //全局区
+
+
+    //全局变量,静态变量,常量
+
+    //创建一个普通的局部变量
+    int a = 10;
+    int b = 10;
+
+    cout <<  << endl;
+    cout <<   << endl;
+}
